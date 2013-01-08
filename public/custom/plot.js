9a72f19cca191eb531d18bb286ad5b039e424534
gem <gem@sw.ru>
2013-01-08 19:24:27 +0400

    initial commit

---
+++ public/custom/plot.js
@@ -0,0 +1,152 @@
+//< needs(dojo)
+
+define(["dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
+	"dojox/charting/Chart", "dojox/charting/themes/Claro",
+	"dojo/store/JsonRest", "dojo/store/Observable",
+	"dojox/charting/StoreSeries",
+	"dojo/text!./plot/plot.html",
+	"dojo/on",
+	"dojox/charting/plot2d/Lines", "dojox/charting/axis2d/Default", "dijit/form/ToggleButton",
+	"dojo/domReady!"],
+    function(declare, WidgetBase, TemplatedMixin, WidgetsInTemplateMixin,
+    		Chart, Claro, JsonRest, Observable, StoreSeries, template,
+    		On) {
+        return declare("plot", [WidgetBase, TemplatedMixin, WidgetsInTemplateMixin], {
+        	sensorId: "",
+        	name: "",
+        	currentValue : "?",
+        	unitOfMeasure: "",
+        	interval: "minute",
+        	updateCurrentValue: true,
+        	templateString: template,
+        	baseClass: "plot",
+        	steps: {minute: 120, hour: 3600, day: 3600*12 },
+        	chart: null,
+        	store: null,
+        	chartInterval: null,
+        	currInterval: null,
+        	postCreate: function(){
+        		this.inherited(arguments);
+        		
+	        	this.updateButtons();
+	        	this.draw();
+	        	this.setTimers();
+	        	if (!this.sensorId)
+	        		return;
+	        	this.createStore();
+	        	this.updateChart();
+	        	this.initCurrValue();
+        	},
+        	setTimers: function() {
+	        	var ths = this;
+				On(this.minuteButton, "click", function() {ths.set('interval', 'minute');});
+				On(this.hourButton, "click", function() {ths.set("interval", 'hour');});
+				On(this.dayButton, "click", function() {ths.set("interval", 'day');});
+
+	        	this.chartInterval = setInterval(function() {
+	        			if (ths.store)
+	        				ths.store.query("?last").forEach(function (res) {
+	        					ths.store.notify(res);
+	        				});
+	        		}, 30000);
+	        	if (this.updateCurrentValue)
+	        		this.currInterval = setInterval(function(){ths.initCurrValue()}, 5000);
+	        },
+	        updateButtons: function() {
+        		this.minuteButton.set('checked', this.interval == 'minute');
+        		this.hourButton.set('checked', this.interval == 'hour');
+        		this.dayButton.set('checked', this.interval == 'day');
+	        },
+	        _setSensorIdAttr: function(id) {
+	        	if (this.sensorId == id)
+	        		return;
+	        	this._set("sensorId", id);
+	        	this.initCurrValue();
+	        	this.createStore();
+	        	this.updateChart();
+	        },
+	        _setIntervalAttr: function (value) {
+	        	if (this.interval == value)
+	        		return;
+	        	this._set("interval", value);
+	        	this.updateButtons();
+	        	this.createStore();
+	        	this.updateChart();
+	        },
+	        _setNameAttr: function(value) {
+	        	this._set("name", value);
+				this.nameNode.innerHTML = value;
+	        },
+	        _setCurrentValueAttr: function(value) {
+	        	this._set("currentValue", value);
+				this.valueNode.innerHTML = value;
+	        },
+	        _setUnitOfMeasureAttr: function(value) {
+	        	this._set("unitOfMeasure", value);
+				this.unitNode.innerHTML = this.unitOfMeasure;
+	        },
+	        createStore: function() {
+	        	this.store = Observable(new JsonRest({
+	        		target: "/sensor/" + this.sensorId + "/value/" + this.interval,
+	        		idProperty: "time"
+	        	}));
+	        },
+	        updateChart: function() {
+	        	this.chart.removeAxis("x");
+	        	var step = this.steps[this.interval] * 1000;
+	        	var addDay = this.interval != 'minute';
+	        	this.chart.addAxis("x", { fixUpper: "major", majorTickStep: step*8, minorTickStep: step,
+	        		labelFunc: function(text, value, precision) {
+	        			var date = new Date(value);
+	        			var res = '';
+	        			if (addDay) {
+	        				var d = date.getDate(); if (d < 9) res += '0';
+	        				res += d + ' ';
+	        			}
+	        			var h = date.getHours(); if (h <9) h = '0' + h;
+	        			var m = date.getMinutes(); if (m <9) m = '0' + m;
+	        			return res + h + ':' + m;
+	        		}
+	        	});
+	        	this.chart.removeSeries("Values");
+				this.chart.addSeries("Values",
+					new StoreSeries(this.store, {}, function (item, store) {
+						return { x: new Date(item["time"]).getTime(), y: item["value"] };
+					}));
+	        	this.chart.render();
+	        },
+	        draw: function () {
+	        	this.chart = new Chart(this.chartNode);
+	        	this.chart.setTheme(Claro);
+	        	this.chart.addPlot("default", {
+	        		type: "Lines",
+	        		markers: false, hAxis: "x", vAxis: "y"
+	        	});
+	        	this.chart.addAxis("y", {fixUpper: "major", fixLower: "major", minorLabels: false, vertical: true, fixed: true, includeZero: true});
+	        },
+	        initCurrValue: function () {
+	        	if (!this.sensorId)
+	        		return;
+	        	if (!this.updateCurrentValue)
+	        		return;
+	        	var curStore = new JsonRest({target: "/sensor/" + this.sensorId + "/value"});
+	        	var ths = this;
+    			curStore.query("?last").forEach(function (res) {
+    				ths.set("currentValue", res.value);
+				});
+	        },
+	        startup: function() {
+	        	this.resize();
+	        },
+        	resize: function(w,h) {
+	        	if (this.chart)
+	        		this.chart.resize(w,h);
+        	},
+        	uninitialize: function() {
+        		this.inherited(arguments);
+        		clearInterval(this.chartInterval);
+        		clearInterval(this.currInterval);
+        	}
+        });
+    }
+);
---
+++ srv/value.js
@@ -0,0 +1,77 @@
+var pg = require('pg'); 
+
+var conString = "tcp://www:1q2w3eQAWSED@router/monitor";
+
+var client = new pg.Client(conString);
+client.connect();
+
+var restify = require('restify');
+
+function send_values(res, next, qstr, params, limit) {
+	if (limit)
+		qstr += " limit " + limit;
+	var query = client.query(qstr, params,
+	  function (err, result) {
+		if (err)
+			return res.send(err);
+		if (result.rows.length > 0)
+			return res.send(result.rows);
+		return next(new restify.ResourceNotFoundError());
+	});
+}
+
+
+var sensorQuery = "SELECT id, name, unit"
+	+ ",(select value from value where value.id=sensor.id order by time desc limit 1) as value"
+	+ " FROM sensor";
+
+function sensor_list(req, res, next) {
+	return send_values(res, next, sensorQuery);
+}
+
+function sensor(req, res, next) {
+	return send_values(res, next, sensorQuery + " WHERE sensor.id = $1", [req.params.id]);
+}
+
+function sensor_values_avg(req, res, next) {
+	switch (req.params.interval) {
+	case 'minute':
+	case 'hour':
+	case 'day':
+		break;
+	default:
+		return next(new restify.InvalidArgumentError(
+			"invalid interval, allowed 'minute', 'hour' and 'day'"));
+	}
+	var qstr = "select id, round(cast(avg(value) as numeric),1) as value, date_trunc($1, t) as time "
+		+ " from value as v (id, t, value) where id = $2 group by id, time order by time desc";
+		
+	send_values(res, next, qstr, [req.params.interval, req.params.id], req.params.last == undefined ? 100 : 1);	
+}
+
+function sensor_values(req, res, next) {
+	var qstr = "SELECT id, time, value FROM value WHERE id = $1 order by time desc";
+	send_values(res, next, qstr, [req.params.id], req.params.last == undefined ? 100 : 1);
+}
+
+var server = restify.createServer();
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.jsonp());
+
+server.get('/sensor', sensor_list);
+server.get('/sensor/:id', sensor);
+server.get('/sensor/:id/value', sensor_values);
+server.get('/sensor/:id/value/:interval', sensor_values_avg);
+
+server.get(/^\/$/, function(req, res, next){
+	res.writeHead(302, {'Location': '/index.html'});
+	res.end();
+});
+server.get(/\/.*\.html/, restify.serveStatic({directory: '../public/'}));
+server.get(/\/.*\.css/, restify.serveStatic({directory: '../public/'}));
+server.get(/\/.*\.js/, restify.serveStatic({directory: '../public/'}));
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
