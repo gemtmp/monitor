7a467c01cc26a553a67171fddf553d5b0d65aff2
Charlie Root <root@router.gem>
2014-04-11 21:11:11 +0400

    use new motherboard sensors

--- scripts/nodestats.pl
+++ scripts/nodestats.pl
@@ -30,7 +30,8 @@ while() {
 #	$t =~ s/C//;
 #	$t =~ s/,/./;
 #	chomp($t);
-#	$h{'router CPU temp'} = $t;
+	$h{'router CPU temp'} = `/usr/local/bin/mbmon -T 2 -c1`;
+	$h{'router CPU fan'} = `/usr/local/bin/mbmon -F 1 -c1`;
 
 	my $all = $dbh->selectall_arrayref("select id from sensor where id like 'online %' group by id");
 	foreach my $i (@{$all}) {
@@ -42,7 +43,7 @@ while() {
 		$id =~ s/://g;
 		$h{"online $id"} = 1;
 	}
-	$h{"router /ext"} = df('/ext')->{'bavail'}/1024/1024;
+	$h{"router /ext"} = int(df('/ext')->{'bavail'}/1024/1024*100)/100;
 
 	my $p = Net::Ping->new('icmp', 1);
 	$h{"internet online"} = $p->ping('google.com') ? 1:0;
